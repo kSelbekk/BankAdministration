b16572044a94d68d386b3873ae4f903f98d8a605
Kevin Selbekk <70540467+kSelbekk@users.noreply.github.com>
2021-04-27 09:02:47 +0200

    datacontext

--- WebApplication7/Models/BankAppDataContext.cs
+++ WebApplication7/Models/BankAppDataContext.cs
@@ -25,15 +25,6 @@ namespace WebApplication7.Models
         public virtual DbSet<Transactions> Transactions { get; set; }
         public virtual DbSet<User> User { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=localhost;Database=BankAppData;Trusted_Connection=True;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
