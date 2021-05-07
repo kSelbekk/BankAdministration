530ccbf02b48d6a1f82bd8a1adf738d1d30aaf33
kSelbekk <70540467+kSelbekk@users.noreply.github.com>
2021-05-07 14:05:06 +0200

    cashier
    edit customer

---
+++ WebApplication7/Controllers/CashierController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplication7.Models;
+using WebApplication7.Services;
+using WebApplication7.ViewModels;
+
+namespace WebApplication7.Controllers
+{
+    public class CashierController :BaseController
+    {
+        public CashierController(BankAppDataContext appDataContext, IBankServices bankServices) 
+            : base(appDataContext, bankServices)
+        {
+        }
+
+        public IActionResult EditCustomer(int id)
+        {
+            var dbCustomer = _bankServices.GetSpecificCustomerInformation(id);
+
+            var viewModel = new CashierEditUserAccountsViewModel
+            {
+                Id = dbCustomer.CustomerId,
+                City = dbCustomer.City,
+                Country = dbCustomer.Country,
+                CountryCode = dbCustomer.CountryCode,
+                EmailAddress = dbCustomer.Emailaddress,
+                Gender = dbCustomer.Gender,
+                Givenname = dbCustomer.Givenname,
+                NationalId = dbCustomer.NationalId,
+                StreetAddress = dbCustomer.Streetaddress,
+                Surname = dbCustomer.Surname,
+                TelephoneCountryCode = dbCustomer.Telephonecountrycode,
+                Telephonenumber = dbCustomer.Telephonenumber,
+                Zipcode = dbCustomer.Zipcode
+            };
+            
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult EditCustomer(CashierEditUserAccountsViewModel viewModel)
+        {
+            if (!ModelState.IsValid) return View(viewModel);
+
+            var dbCustomer = _bankServices.GetSpecificCustomerInformation(viewModel.Id);
+
+            dbCustomer.Givenname = viewModel.Givenname;
+            dbCustomer.Country = viewModel.Country;
+            dbCustomer.CountryCode = viewModel.CountryCode;
+            dbCustomer.NationalId = viewModel.NationalId;
+            dbCustomer.City = viewModel.City;
+            dbCustomer.Surname = viewModel.Surname;
+            dbCustomer.Gender = viewModel.Gender;
+            dbCustomer.Telephonenumber = viewModel.Telephonenumber;
+            dbCustomer.Telephonecountrycode = viewModel.TelephoneCountryCode;
+            dbCustomer.Zipcode = viewModel.Zipcode;
+            dbCustomer.Emailaddress = viewModel.EmailAddress;
+
+            _appDataContext.SaveChanges();
+
+            return RedirectToAction("CustomerProfile", "Customer", new {id = dbCustomer.CustomerId});
+        }
+
+    }
+}
---
+++ WebApplication7/ViewModels/CashierEditUserAccountsViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApplication7.ViewModels
+{
+    public class CashierEditUserAccountsViewModel
+    {
+        public int Id { get; set; }
+        [MaxLength(256), Required]
+        public string Givenname { get; set; }
+        [MaxLength(256), Required]
+        public string Surname { get; set; }
+        [MaxLength(256), Required]
+        public string StreetAddress { get; set; }
+
+        
+        public string Gender { get; set; }
+        public SelectListItem SelectedGender { get; set; }
+
+        [MaxLength(256), Required]
+        public string City { get; set; }
+        [MaxLength(50), Required]
+        public string Zipcode { get; set; }
+        [MaxLength(256), Required]
+        public string Country { get; set; }
+        [MaxLength(256), Required]
+        public string CountryCode { get; set; }
+        [MaxLength(50)]
+        public string? NationalId { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string? TelephoneCountryCode { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string? Telephonenumber { get; set; }
+        [EmailAddress]
+        public string? EmailAddress { get; set; }
+    }
+}
